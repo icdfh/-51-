562a2cbdd87fd516a49d0b01245b640f28df0c49
Джетиберген Мадияр <madiyar.jetibergen@narxoz.kz>
2026-03-10 18:48:51 +0500

    practice

---
+++ fifthlesson/main.cpp
@@ -0,0 +1,137 @@
+// Вложенные циклы
+// 1.
+	// for(int i = 1; i <= 3; i++){
+	// 	for(int j = 1; j <= 2 ; j++){
+	// 		cout<<"i = " << i << ", j = " << j << endl;
+	// 	}
+	// }
+	
+	// 2.
+	// for(int i = 1; i <= 3; i++){
+	// 	for( int j = 1; j <= 4; j++){
+	// 		cout << i << j << " ";
+	// 	}
+	// 	cout<< endl;
+	// }
+	// 3.
+	// for(int i = 1; i <= 3; i++){
+	// 	for(int j = 1; j <= 4; j++){
+	// 		cout<< i << 0;
+	// 	}
+	// 	cout<<endl;
+	// }
+	// for (int i = 1; i <= 3; i++){
+	// 	for(int j = 1; j <= 3; j++){
+	// 		cout<<"(" << i << "," << j << ")";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for(int i = 1; i <= 5; i++){
+	// 	for(int j = 1; j <= 5; j++){
+	// 		cout<<"* ";
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	// int cols = 3;
+	// int rows = 7;
+
+	// for(int i = 1; i <= cols;i++){
+	// 	for( int j = 1; j <= rows; j++){
+	// 		cout << "* ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for (int i =1; i<=5; i++){
+	// 	for(int j = 1; j <= i; j++){
+	// 		cout<<"*";
+	// 	}
+	// 	cout<< endl;
+	// }
+
+	// for(int i = 5; i >= 1; i--){
+	// 	for(int j = 1; j <= i; j++){
+	// 		cout<<"* ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for(int i = 1; i <= 5; i++){
+	// 	for(int j = 1; j <= i; j++){
+	// 		cout<< j << " ";
+	// 	}
+	// 	cout<< endl;
+	// }
+
+	// for(int i = 1; i <= 5; i++){
+	// 	for(int j = 1; j <= 5; j++){
+	// 		cout << i * j << "\t";
+	// 	}
+	// 	cout<< endl;
+	// }
+
+	// for(int i = 1; i <= 5; i++){
+	// 	for(int s = 1; s <= 5 - i; s++){
+	// 		cout<< " ";
+	// 	}
+	// 	for(int j = 0; j <= i; j++){
+	// 		cout<<"* ";
+	// 	}
+	// 	cout<< endl;
+	// }
+
+	// for(int i = 5; i >= 1; i--){
+	// 	for(int s = 1; s <= 5 - i; s++){
+	// 		cout<<" ";
+	// 	}
+	// 	for(int j = 1; j <= 2 * i -1;j++){
+	// 		cout<<"*";
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	// for(int i = 1; i <= 5; i++){
+	// 	for(int s = 1; s <= 5 - i; s++){
+	// 		cout<<" ";
+	// 	}
+		
+	// 	for (int j = 1; j <= 2 * i -1; j++){
+	// 		cout<<"*";
+	// 	}
+	// 	cout << endl;
+	// }
+	// 	for(int i = 4; i >= 1; i--){
+	// 		for(int s = 1; s <= 5 - i; s++){
+	// 			cout<< " ";
+	// 		}
+	// 		for(int j = 1; j <= 2 * i - 1; j++){
+	// 			cout<<"*";
+	// 		}
+	// 		cout<<endl;
+	// 	}
+
+	// for(int i = 1; i <=5; i++){
+	// 	for(int j = 1; j <= 5; j++){
+	// 		if(i == 1 || i == 5 || j == 1 || j == 5){
+	// 			cout<<"* ";
+	// 		}
+	// 		else{
+	// 			cout<< "  ";
+	// 		}
+	// 	}
+	// 	cout<<endl;
+	// }
+	for(int i = 1; i <= 8; i++){
+		for(int j = 1; j <= 8; j++){
+			if((i + j) % 2 == 0){
+				cout << "⬛";
+			}
+			else{
+				cout<<"⬜";
+			}
+		}
+		cout<<endl;
+	}
+}
