c577e3d851e2322f039a7b0b64254e54b4d0fde8
Джетиберген Мадияр <madiyar.jetibergen@narxoz.kz>
2026-03-24 18:20:02 +0500

    HW AND EXAMPLES

--- seventhlesson/main.cpp
+++ seventhlesson/main.cpp
@@ -1,6 +1,6 @@
-// Двумерные массивы и рандомайзер
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
@@ -45,21 +45,62 @@ int main(){
 	// 	cout<<endl;
 	// }
 
-	int arr[2][3];
+	// int arr[2][3];
 
-	for(int i = 0; i < 2; i++){
-		for(int j = 0; j < 3; j++){
-			cout<<"Введите элемент [" << i << "]["<<j<<"]: ";
-			cout<<i<<j<<endl;
-			cin>>arr[i][j];
+	// for(int i = 0; i < 2; i++){
+	// 	for(int j = 0; j < 3; j++){
+	// 		cout<<"Введите элемент [" << i << "]["<<j<<"]: ";
+	// 		cout<<i<<j<<endl;
+	// 		cin>>arr[i][j];
+	// 	}
+	// }
+	// for(int i = 0; i < 2; i++){
+	// 	for(int j = 0; j < 3;j++){
+	// 		cout<<arr[i][j]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	// srand(time(0));
+
+	// for(int i = 0; i <= 5; i++){
+	// 	cout<< rand() <<endl;
+	// }
+
+	// srand(time(0));
+	// for(int i = 1; i <= 10; i++){
+	// 	cout << rand() % 100 << " ";
+	// }
+
+	// srand(time(0));
+	// for(int i = 1; i <= 10; i++){
+	// 	cout << rand() % 11 + 5 << " ";
+	// }
+
+	// int arr[5];
+	// srand(time(0));
+
+	// for(int i = 0; i < 5; i++){
+	// 	arr[i] = rand() % 10 + 1;
+	// }
+	// for(int i = 0; i < 5; i++){
+	// 	cout<<arr[i] << " ";
+	// }
+
+	int arr[3][4];
+	srand(time(0));
+
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 4; j++){
+			arr[i][j] = rand() % 10 + 1;
 		}
-	}
-	for(int i = 0; i < 2; i++){
-		for(int j = 0; j < 3;j++){
-			cout<<arr[i][j]<<" ";
+	}	
+
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 4; j++){
+			cout << arr[i][j] << "\t";
 		}
 		cout<<endl;
 	}
 	
 }
-
